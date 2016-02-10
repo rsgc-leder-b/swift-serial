a8d840c04168296d6f951b3d7e914670c62a22bb
rsgc-leder-b <bleder@rsgc.on.ca>
2016-02-10 18:33:22 -0500

    Added pong and Arduino communication

---
+++ Pong-Animation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pong-Animation
+//
+//  Created by Brendan Leder on 2015-12-14.
+//  Copyright © 2015 Russell Gordon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
